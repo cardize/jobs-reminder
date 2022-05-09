a5d64e63e5fb500f4ae0f3d8faf354e1dd07f989
cardize <mustafasamisakarya@gmail.com>
2022-05-09 13:56:00 +0300

    header done template done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './styles.scss'
+import logo from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Jobs</h1>
+    <div className="main-container">
+      <div className="header-container">
+        <img className="logo" src={logo} alt="Google Logo" />
+        <h1>Jobs Reminder</h1>
+      </div>
+      <div className="body-container">
+        <div className="create-job">
+          <h1>Create Job</h1>
+        </div>
+
+        <div className="list-job">
+          <h1>List Job</h1>
+        </div>
+      </div>
+      <div className="footer-container">
+        <h1>Footer</h1>
+      </div>
     </div>
   )
 }
