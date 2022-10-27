fc8204e7faa5385a619113a08932c12f81519743
Mustafa Sami Sakarya <mustafasamisakarya@gmail.com>
2022-10-28 02:22:57 +0300

    edit-modal & delete modal added

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -1,9 +1,13 @@
 import Pagination from './Pagination'
 import Header from './Header'
 import Footer from './Footer'
+import DeleteModal from './DeleteModal'
+import EditModal from './EditModal'
 
 export {
     Header,
     Footer,
     Pagination,
+    DeleteModal,
+    EditModal,
 }
